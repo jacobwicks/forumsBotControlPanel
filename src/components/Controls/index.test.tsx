e912eba57bea6d8eb5996e6b606fbcf467eabb88
jacobwicks <jacob.m.wicks@gmail.com>
2020-05-18 18:46:08 -0700

    controls interval

--- src/components/Controls/index.test.tsx
+++ src/components/Controls/index.test.tsx
@@ -1,7 +1,7 @@
 //test the controls page of the control panel
 //React lets us create and display components to the user
 //We need to import it so that we can look at the components to test them
-import React, { useContext } from 'react';
+import React from 'react';
 
 //testing library gives us methods to test components
 //we use render to look at React components
@@ -118,8 +118,138 @@ describe('start and stop buttons', () => {
 });
 
 it('tells the user if the bot is currently running', () => {
-    const { getByText } = render(<Controls />);
+    const { getByText } = renderControls();
     const botStateHeader = getByText('bot state', { exact: false });
 
     expect(botStateHeader).toBeInTheDocument();
+});
+
+describe('interval', () => {
+    it('has an interval display', () => {
+        const { getByText } = renderControls();
+        const intervalDisplay = getByText(/runs every/i);
+
+        expect(intervalDisplay).toBeInTheDocument();
+    });
+
+    it('displays the current interval', () => {
+        const { getByText } = renderControls();
+        const intervalDisplay = getByText(/runs every/i);
+        const interval = initialState.interval;
+        expect(intervalDisplay).toHaveTextContent(interval.toString());
+    });
+
+    describe('set interval input', () => {
+        const intervalState = { ...initialState, interval: 5 };
+
+        it('has an interval input', () => {
+            const { getByPlaceholderText } = renderControls();
+            const intervalInput = getByPlaceholderText(/set interval/i);
+            expect(intervalInput).toBeInTheDocument();
+        });
+
+        it('sets the interval value', () => {
+            const { getByPlaceholderText, getByText } = renderControls(
+                intervalState
+            );
+            const intervalDisplay = getByText(/runs every/i);
+            expect(intervalDisplay).toHaveTextContent(
+                intervalState.interval.toString()
+            );
+
+            const intervalInput = getByPlaceholderText(/set interval/i);
+            //get a reference to the first child of element
+            //const innerIntervalInput = intervalInput.children[0];
+
+            const newInterval = '7';
+
+            fireEvent.change(intervalInput, {
+                target: { value: newInterval },
+            });
+
+            expect(intervalInput).toHaveValue(newInterval);
+
+            expect(intervalDisplay).toHaveTextContent(newInterval);
+        });
+
+        describe("won't set invalid intervals", () => {
+            const invalidIntervals = [0, 1, -1, -5];
+
+            test.each(invalidIntervals)(
+                //printing the title uses 'printf syntax'. numbers are %d, not %n
+                "won't set invalid interval %d",
+                //name the arguments, same order as in the array we generated
+                (interval) => {
+                    //render selector with the state with the subjects
+                    const { getByPlaceholderText, getByText } = renderControls(
+                        intervalState
+                    );
+                    const intervalInput = getByPlaceholderText(/set interval/i);
+
+                    fireEvent.change(intervalInput, {
+                        target: { value: interval.toString() },
+                    });
+
+                    const intervalDisplay = getByText(/runs every/i);
+
+                    expect(intervalDisplay).toHaveTextContent(
+                        intervalState.interval.toString()
+                    );
+                }
+            );
+        });
+    });
+
+    describe('increase and decrease buttons', () => {
+        it('increments interval', () => {
+            const { getByTestId, getByText } = renderControls();
+            const intervalDisplay = getByText(/runs every/i);
+            const increaseButton = getByText(/increase/i);
+
+            expect(intervalDisplay).toHaveTextContent(
+                initialState.interval.toString()
+            );
+
+            increaseButton.click();
+
+            expect(intervalDisplay).toHaveTextContent(
+                (initialState.interval + 1).toString()
+            );
+        });
+
+        describe('decrements interval', () => {
+            it('decrements by 1', () => {
+                const { getByText } = renderControls();
+                const interval = getByText(/runs every/i);
+                const decreaseButton = getByText(/decrease/i);
+
+                expect(interval).toHaveTextContent(
+                    initialState.interval.toString()
+                );
+
+                decreaseButton.click();
+
+                expect(interval).toHaveTextContent(
+                    (initialState.interval - 1).toString()
+                );
+            });
+
+            it("won't decrement to invalid interval", () => {
+                const lowIntervalState = { ...initialState, interval: 3 };
+                const { getByText } = renderControls(lowIntervalState);
+                const interval = getByText(/runs every/i);
+                const decreaseButton = getByText(/decrease/i);
+
+                expect(interval).toHaveTextContent('3');
+
+                decreaseButton.click();
+
+                expect(interval).toHaveTextContent('2');
+
+                decreaseButton.click();
+
+                expect(interval).toHaveTextContent('2');
+            });
+        });
+    });
 });
--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
-import { Button, Message } from 'semantic-ui-react';
+import { Button, Input, Message } from 'semantic-ui-react';
 import { BotContext } from '../../services/BotContext';
 import { BotActionTypes } from '../../types';
 
+//next: Add interval, setInterval Input, increase/decrease interval, interval in hrs?
 const Controls = () => {
-    const { dispatch, running } = useContext(BotContext);
+    const { dispatch, interval, running } = useContext(BotContext);
 
     return (
         <div>
@@ -12,18 +13,49 @@ const Controls = () => {
                 <Message.Header>Bot State</Message.Header>
                 <p>{!running && 'Not '}Running</p>
             </Message>
-            <Button
-                onClick={() => dispatch({ type: BotActionTypes.start })}
-                color="green"
-            >
-                Start
-            </Button>
-            <Button
-                onClick={() => dispatch({ type: BotActionTypes.stop })}
-                color="red"
-            >
-                Stop
-            </Button>
+            <div>
+                <Button
+                    onClick={() => dispatch({ type: BotActionTypes.start })}
+                    color="green"
+                >
+                    Start
+                </Button>
+                <Button
+                    onClick={() => dispatch({ type: BotActionTypes.stop })}
+                    color="red"
+                >
+                    Stop
+                </Button>
+            </div>
+            <Message>
+                <Message.Header>Interval</Message.Header>
+                <p>Runs Every {interval} Minutes</p>
+            </Message>
+            <div>
+                <Button
+                    onClick={() =>
+                        dispatch({ type: BotActionTypes.increaseInterval })
+                    }
+                >
+                    Increase
+                </Button>
+                <Button
+                    onClick={() =>
+                        dispatch({ type: BotActionTypes.decreaseInterval })
+                    }
+                >
+                    Decrease
+                </Button>
+                <Input
+                    onChange={(e, { value }: { value: string }) =>
+                        dispatch({
+                            type: BotActionTypes.setInterval,
+                            interval: Number(value),
+                        })
+                    }
+                    placeholder="Set Interval..."
+                />
+            </div>
         </div>
     );
 };
